000220fd63583302fcbbc07cc6ed4dbb4945c953
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-11-22 15:50:32 -0300

    Abertura do projeto

---
+++ src/main/java/com/mercadolivro/MercadoLivroApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoLivroApplication.class, args);
+	}
+
+}
