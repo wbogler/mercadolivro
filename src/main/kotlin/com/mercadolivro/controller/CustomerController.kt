b2b61c5ac2577a13690ccfaaef493792d1714247
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-11-26 17:38:57 -0300

    inserção controller put

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -28,6 +29,17 @@ class CustomerController {
     @GetMapping("/{id}")
     fun getCustomer(@PathVariable id:Long): ResponseEntity<CustomerModel>{
         return ResponseEntity.ok(customers1.filter { it.id == id }.first())
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun updateCustomer(@PathVariable id:Long, @RequestBody customer: CustomerModel): ResponseEntity<CustomerModel> {
+       customers1.filter { it.id == id }.first().let {
+            it.id = id
+            it.name = customer.name
+            it.email = customer.email
+        }
+        return ResponseEntity.ok( customers1.filter { it.id == id }.first())
     }
 
     @PostMapping
