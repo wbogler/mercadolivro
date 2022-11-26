eeffb84ce95a98d3aedd66d32d8002fa80f36b74
Willian Bogler Da Silva <willian.silva@pti.org.br>
2022-11-26 14:33:26 -0300

    adição do http status no post

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,11 +1,13 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,6 +21,7 @@ class CustomerController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun posttCustomers(@RequestBody customer:CustomerModel):CustomerModel{
 
         return customer
